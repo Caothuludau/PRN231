ec424678213f349e89776481d092e156c3998067
CaoThuLuDau <npl.working@gmail.com>
2024-05-08 17:23:50 +0700

    Slot 1 demo console app

---
+++ Slot 1/Demo 2/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2
+{
+    internal class Course
+    {
+        public int ID {  get; set; }
+        public string title { get; set; }
+        public DateTime startdate { get; set; }
+        public Course() { }
+        public Course(int ID, string title, DateTime startdate) {
+            this.title = title;
+            this.startdate = startdate;
+            this.ID = ID;
+        }
+    }
+}
---
+++ Slot 1/DemoRecode/Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args) 
+    {
+        Console.WriteLine("Nhập vào số nguyên dương a:");
+        int a = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập vào số nguyên dương b:");
+        int b = Int32.Parse(Console.ReadLine());
+
+        //tinh tong
+        Console.WriteLine($"Sum = {a+b}");
+
+    }
+}
