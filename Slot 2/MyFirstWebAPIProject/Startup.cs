7e208277ffc6ad4920bc7f2bdef206ec6840f5c6
CaoThuLuDau <npl.working@gmail.com>
2024-05-10 15:40:12 +0700

    Update Slot 2 Demo - RESTful API

---
+++ Slot 2/MyFirstWebAPIProject/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace MyFirstWebAPIProject
+{
+    public class Startup
+    {
+    }
+}
---
+++ Slot 2/Slot 2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Slot_2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //Request nao bat dau bang controller thi se nhay vao day
+        // VD: /WeatherForecast
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")] //Config voi controller nay, Neu method la GET -> se xu ly Get...
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
